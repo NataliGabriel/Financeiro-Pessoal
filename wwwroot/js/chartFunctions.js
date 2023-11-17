ee644a498570d22531f429d9cc548b3a9825b4a0
Gabriel Natali Gonçalves <bielnatali7@gmail.com>
2023-11-17 16:23:31 -0300

    Add files via upload

---
+++ wwwroot/js/chartFunctions.js
@@ -0,0 +1,19 @@
+﻿window.renderChart = async function () {
+    // Obter os dados do gráfico - substitua essa lógica com seus próprios dados
+    var data = google.visualization.arrayToDataTable([
+        ['Categoria', 'Valor'],
+        ['Categoria A', 20],
+        ['Categoria B', 30],
+        ['Categoria C', 50],
+    ]);
+
+    // Configurações do gráfico
+    var options = {
+        title: 'Meu Gráfico',
+        pieHole: 0.4,
+    };
+
+    // Renderizar o gráfico
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+}
